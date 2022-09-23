fe056e48e378a62c46ba5c0c96a51ab59d50e9a8
Lu Nguyen <nguyentnlu@gmail.com>
2022-09-23 12:04:30 +0700

    Update images size

--- resources/views/pages/article/show.blade.php
+++ resources/views/pages/article/show.blade.php
@@ -23,9 +23,6 @@
 </div>
 <div class="container-fluid pb-4 pt-5">
     <div class="container animate-box">
-        <div>
-            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Trending</div>
-        </div>
         <div class="owl-carousel owl-theme" id="slider2">
             @foreach ($articles as $article)
             <div class="item px-2">
